ab0ca57ea6248db2a61f0ac481686a32c5c23c3e
darshan75462020-eng <darshan75462020@gmail.com>
2026-03-15 22:50:15 +0530

    added changes

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://ai-tech-blog-gilt.vercel.app/sitemap.xml',
   };
 }
